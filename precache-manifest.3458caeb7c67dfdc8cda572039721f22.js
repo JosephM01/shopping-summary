7e7255685ad822de0be0423f3bc05e7f6aa77d16
Joseph <josephwow1@gamil.com>
2019-05-09 19:15:02 -0300

    Updates

---
+++ precache-manifest.3458caeb7c67dfdc8cda572039721f22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4648df86bc7e7c7d4caa7bcdad2fba65",
+    "url": "/shopping-summary/index.html"
+  },
+  {
+    "revision": "3378550781ac6fbeccd8",
+    "url": "/shopping-summary/static/css/main.7f0b972d.chunk.css"
+  },
+  {
+    "revision": "bf2a39df79992331cd85",
+    "url": "/shopping-summary/static/js/2.387af9af.chunk.js"
+  },
+  {
+    "revision": "3378550781ac6fbeccd8",
+    "url": "/shopping-summary/static/js/main.5094a781.chunk.js"
+  },
+  {
+    "revision": "043131627f9ee91296aa",
+    "url": "/shopping-summary/static/js/runtime~main.62ecc992.js"
+  }
+]);
